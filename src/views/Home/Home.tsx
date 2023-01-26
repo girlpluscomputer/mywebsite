fac789f44f4333e9f949847bb61c7eca7dc77fea
girlpluscomputer <samantha.ruth.nunez@gmail.com>
2023-01-26 16:15:28 +0100

    Initial setup

---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="bg-black w-full h-screen">
+      <h1 className="text-white">Hi!, I’m Sam</h1>
+      <p className="text-white">Turning ideas into real life products</p>
+    </div>
+  )
+}
+
+export default Home
